382fdd3493dcf88dbaef0f76b815622df10bb42f
May Jawdat <may.jawdat@gmail.com>
2017-03-21 10:06:12 -0700

    Add user controller, pseudocode

---
+++ app/controllers/index.rb
@@ -0,0 +1,13 @@
+get '/' do
+  "Num num cookies."
+end
+
+# get new order form
+# post new order form
+# order confirmation
+# order status
+
+# get new user sign up
+# post new user sign up
+# user sign in
+# user profile
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  create_table :users do |t|
+    t.string :email, null: false
+    t.string :password_hash, null: false
+    t.string :delivery_address
+    t.string :billing_address
+    t.string :phone_number
+
+    t.timestamps
+  end
+
 end
